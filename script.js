d124b85b7309a9932c754ee9ca0b74168a02c2c3
UniqueJackal5 <36040284+UniqueJackal5@users.noreply.github.com>
2025-04-05 23:26:55 +0545

    adding ball game update

--- script.js
+++ script.js
@@ -95,3 +95,35 @@ ctx.fill();
 ctx.stroke();
 
 
+//let's try animate feature
+let x = 100;
+let y = 100;
+let dx = 2;
+let dy = 2;
+const radius = 20;
+
+function update(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+
+    //draw the ball
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+
+    //move the ball by dx and dy
+    x +=  dx;
+    y +=  dy;
+
+    //let's make the ball bouncy on the edges
+    if(x + radius >canvas.width || x-radius < 0){
+        dx = -dx;
+    }
+    if(y + radius > canvas.height || y - radius < 0){
+        dy = -dy;
+    }
+    requestAnimationFrame(update);
+}
+
+update();
